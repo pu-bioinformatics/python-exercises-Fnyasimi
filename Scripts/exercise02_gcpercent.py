d564274530c9c27536fc886baf2c4bc45d049ea9
User <user@BioInfo3>
2019-03-11 16:45:02 +0300

    The script is used to calculate the GC percentage content and length of a DNA sequence

---
+++ Scripts/exercise02_gcpercent.py
@@ -0,0 +1,9 @@
+#! /home/user/anaconda2/envs/bioinf/bin/python
+print("Input your DNA sequence: ")
+
+dna = input()
+dna = dna.upper()
+GC_percent=((dna.count('C') + dna.count('G'))/len(dna) * 100)
+AT_percent=((dna.count('A') + dna.count('T'))/len(dna) * 100)
+print("The DNA has a GC percentage of %.3f and AT percentage of %.3f, the length of DNA is %i." \
+	%(GC_percent, AT_percent, len(dna)))
